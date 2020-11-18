7df9b1e8165be9e09b99b3b4656fd21321e24697
lorenzo.lizzo <lorenzo.lizzo.stud@ispascalcomandini.it>
2020-11-18 10:47:23 +0100

    Passo 1: aggiunte le variabili private

---
+++ EsVerificaGithub/Libro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsVerificaGithub
+{
+    class Libro
+    {
+        private string _autore;
+        private string _titolo;
+        private int _annoPubblicazione;
+        private string _editore;
+        private int _numeroPagine;
+    }
+}
