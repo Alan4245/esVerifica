32cc0334f0323477cbf2dff90eb1a97d5c37e3ec
filippo.bianconi <filippo.bianconi.stud@ispascalcomandini.it>
2020-11-18 10:48:21 +0100

    creazione classe Biblioteca

---
+++ EsVerificaGithub/Biblioteca.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsVerificaGithub
+{
+    class Biblioteca
+    {
+        string _nome;
+        string _indirizzo;
+        float _orarioApertura;
+        float _orarioChiusura;
+
+
+        public Biblioteca(string Nome, string indirizzo,float orarioApertura, float orarioChiusura, List <string > Libri)
+        {
+            Nome = _nome;
+            indirizzo = _indirizzo;
+            orarioApertura = _orarioApertura;
+            orarioChiusura = _orarioChiusura;
+
+        }
+
+
+
+    }
+}
